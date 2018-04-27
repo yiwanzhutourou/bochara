6266ef2a0ad675b5ca92dddde9d9cddcbbbce391
cuiyis@163.com <cuiyis@163.com>
2018-04-27 18:59:47 +0800

    阿里云短信接口搞定

---
+++ youdu/app/Lib/SMS/AliSmsSender.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Lib\SMS;
+
+use Mrgoon\AliSms\AliSms;
+
+class AliSmsSender {
+    public static function sendBorrowBookSms($phoneNumber, $fromUserName, $bookName) {
+        $params = [
+            'from_user_name'   => $fromUserName,
+            'borrow_book_name' => $bookName
+        ];
+        $sms = new AliSms();
+        $response = $sms->sendSms(
+            $phoneNumber, 'SMS_76385138', $params);
+        if (!$response && $response->Code === 'OK') {
+            return true;
+        }
+        // TODO 通知一下，防止线上大量短信发送失败
+        return false;
+    }
+
+    public static function sendVeriCodeSms($phoneNumber, $code) {
+        $params = [
+            'verify_code'   => $code
+        ];
+        $sms = new AliSms();
+        $response = $sms->sendSms(
+            $phoneNumber, 'SMS_76440167', $params);
+        if (!$response && $response->Code === 'OK') {
+            return true;
+        }
+        return false;
+    }
+
+    public static function sendNewFeatureSms($phoneNumber, $newFeature) {
+        $params = [
+            'new_feature' => $newFeature,
+        ];
+        $sms = new AliSms();
+        $response = $sms->sendSms(
+            $phoneNumber, 'SMS_89945049', $params);
+        if (!$response && $response->Code === 'OK') {
+            return true;
+        }
+        return false;
+    }
+
+}
--- youdu/config/app.php
+++ youdu/config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Aliyun sms
+        Mrgoon\AliSms\ServiceProvider::class,
+
     ],
 
     /*
